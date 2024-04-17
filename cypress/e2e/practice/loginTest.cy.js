c6ec2548bec72ee08f01b6f82a449c5926aa68a1
Cole Dapprich <98425368+cdapprich@users.noreply.github.com>
2024-04-17 13:48:10 -0500

    Update loginTest.cy.js

--- cypress/e2e/practice/loginTest.cy.js
+++ cypress/e2e/practice/loginTest.cy.js
@@ -1,11 +1,4 @@
 it('simple login', () => {
-   //cy.get('#Domain').type('Conner Swanson Test Domain');
-   //cy.get('#Domain').type('campus1.stresstest.tx');
-   //cy.get('#Password').type('password');
-   //cy.get('#Password').type('LM94&#wV!Z');
-   //cy.get('#Username').type('erbot0001');
-   //cy.get('#Username').type('test_000001');
-
    const userName = 'erbot0001'
    const password = 'password'
    const domain = 'Conner Swanson Test Domain'
